667a7b254be2b6e566ee79d85e87efacd74fdb30
Insaf MIM <misterinsh@gmail.com>
2025-09-06 16:43:00 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+
+// Popup
+document.getElementById("openPopup").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "flex";
+});
+
+document.getElementById("closePopup").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "none";
+});
